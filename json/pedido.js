92fb0df7a399c6a6fcc9f59cd8e70d33b8c63756
Elton Barbosa Rodrigues <eltonbr.barbosa@gmail.com>
2016-08-28 21:25:44 -0300

    JSON exemplo pedidos

---
+++ json/pedido.js
@@ -0,0 +1,65 @@
+{
+	"pedidos": [{
+		"id": "001",
+		"dataEmissao": "",
+		"vendedor": "",
+		"frete": "",
+		"transportadora": "",
+		"status": "",
+		"cliente": {
+			"id": 1,
+			"nome": "",
+			"razaoFantazia": "",
+			"telefone": "",
+			"email": "",
+			"cpf": "",
+			"cnpj": "",
+			"inscricaoEstadual": "",
+			"endereco": {
+				"rua": "",
+				"numero": "",
+				"bairro": "",
+				"cep": "",
+				"cidade": "",
+				"estado": ""
+			}
+		},
+		"produtos": [{
+			"codigo": "",
+			"descricao": "",
+			"quantidade": "",
+			"precoUnitario": ""
+		}]
+	}, {
+		"id": "002",
+		"dataEmissao": "",
+		"vendedor": "",
+		"frete": "",
+		"transportadora": "",
+		"status": "",
+		"cliente": {
+			"id": 2,
+			"nome": "",
+			"razaoFantazia": "",
+			"telefone": "",
+			"email": "",
+			"cpf": "",
+			"cnpj": "",
+			"inscricaoEstadual": "",
+			"endereco": {
+				"rua": "",
+				"numero": "",
+				"bairro": "",
+				"cep": "",
+				"cidade": "",
+				"estado": ""
+			}
+		},
+		"produtos": [{
+			"codigo": "",
+			"descricao": "",
+			"quantidade": "",
+			"precoUnitario": ""
+		}]
+	}]
+}
